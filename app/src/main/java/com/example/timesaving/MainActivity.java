3a641bad1379f676e8813780ff02716b9fb7c292
PC-Vecta <christian.pn@live.com>
2022-10-04 17:26:38 -0500

    Avance 01

--- app/src/main/java/com/example/timesaving/MainActivity.java
+++ app/src/main/java/com/example/timesaving/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.timesaving;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnRegistrar=findViewById(R.id.button2);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2Registrarse.class);
+            /*  intent.putExtras(enviadato);*/
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
